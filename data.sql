5342ac6e708801ed7ad5287e967662f084387a43
fherrerao <fherrera0206@gmail.com>
2022-03-21 16:52:07 -0500

    Populate database with sample data

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Agumon', '03-02-2020', 0, true, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Gabumon', '15-11-2018', 2, true, 8);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Pikachu', '07-01-2021', 1, false, 15.04);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Devimon', '12-05-2017', 5, true, 11);
