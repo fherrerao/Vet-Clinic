8317888cfb28551f8ac22a9b489b834be74b8bc3
fherrerao <fherrera0206@gmail.com>
2022-03-21 16:20:44 -0500

    Create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals (    
+	id 	INT 		NOT NULL,
+	name TEXT		NOT NULL,
+	date_of_birth 	date,
+	escape_attempts	INT,
+	neutered			BOOLEAN,
+	weigth_kg		DECIMAL
 );
