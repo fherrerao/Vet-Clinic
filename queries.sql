a81a5059c9a6ecbde9e5be146fcd05a9b18f89b4
fherrerao <fherrera0206@gmail.com>
2022-03-22 17:12:58 -0500

    Use of savepoint

--- queries.sql
+++ queries.sql
@@ -8,3 +8,51 @@ SELECT name, escape_attempts FROM animals WHERE weigth_kg > 10.5;
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weigth_kg BETWEEN 10.4 AND 17.3;
+
+/*MILESTONE-2*/
+
+/*Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that species columns went back to the state before transaction.*/
+
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+
+
+/*Inside a transaction:
+Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+Commit the transaction.
+Verify that change was made and persists after commit.*/
+
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+UPDATE animals SET species = 'pokemon' WHERE species is NULL;
+COMMIT;
+SELECT * FROM animals;
+
+/*Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
+After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;)*/
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+
+/*Inside a transaction:
+Delete all animals born after Jan 1st, 2022.
+Create a savepoint for the transaction.
+Update all animals' weight to be their weight multiplied by -1.
+Rollback to the savepoint
+Update all animals' weights that are negative to be their weight multiplied by -1.
+Commit transaction*/
+
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '01-01-2022';
+SAVEPOINT SP1;
+UPDATE animals SET weigth_kg = weigth_kg *- 1;
+ROLLBACK TO SP1;
+UPDATE animals SET weigth_kg = weigth_kg * -1 WHERE weigth_kg < 0;
+COMMIT;
