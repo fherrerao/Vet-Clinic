4606de9d4fed428403b5d81f41c346ba855ddac9
fherrerao <fherrera0206@gmail.com>
2022-03-22 17:56:51 -0500

    Add titles to each query

--- queries.sql
+++ queries.sql
@@ -11,51 +11,66 @@ SELECT * FROM animals WHERE weigth_kg BETWEEN 10.4 AND 17.3;
 
 /*MILESTONE-2*/
 
-/*Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that species columns went back to the state before transaction.*/
-
+-- Inside a transaction update the animals table by setting the species column to unspecified. 
 BEGIN;
 UPDATE animals SET species = 'unspecified';
+
+-- Verify that change was made. 
 SELECT * FROM animals;
+
+-- Then roll back the change and verify that species columns went back to the state before transaction.
 ROLLBACK;
+SELECT * FROM animals;
 
 
 
-/*Inside a transaction:
-Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
-Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
-Commit the transaction.
-Verify that change was made and persists after commit.*/
-
+-- Inside a transaction:
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
 UPDATE animals SET species = 'pokemon' WHERE species is NULL;
+
+-- Commit the transaction.
 COMMIT;
+
+-- Verify that change was made and persists after commit.
 SELECT * FROM animals;
 
-/*Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
-After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;)*/
 
+-- Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
 BEGIN;
 DELETE FROM animals;
 ROLLBACK;
+
+-- After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;)
 SELECT * FROM animals;
 
 
-/*Inside a transaction:
-Delete all animals born after Jan 1st, 2022.
-Create a savepoint for the transaction.
-Update all animals' weight to be their weight multiplied by -1.
-Rollback to the savepoint
-Update all animals' weights that are negative to be their weight multiplied by -1.
-Commit transaction*/
 
+-- Inside a transaction:
+-- Delete all animals born after Jan 1st, 2022.
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '01-01-2022';
+
+-- Create a savepoint for the transaction.
 SAVEPOINT SP1;
+
+-- Update all animals' weight to be their weight multiplied by -1.
 UPDATE animals SET weigth_kg = weigth_kg *- 1;
+
+-- Rollback to the savepoint
 ROLLBACK TO SP1;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
 UPDATE animals SET weigth_kg = weigth_kg * -1 WHERE weigth_kg < 0;
+
+-- Commit transaction
 COMMIT;
+
+
+
 
 -- Write queries to answer the following questions:
 -- How many animals are there?
