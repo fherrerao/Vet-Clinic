e55233f4c5bb035b63a90b8fbe684a487ebed33d
fherrerao <fherrera0206@gmail.com>
2022-03-23 13:01:32 -0500

    Add Foreign key to animals table

--- data.sql
+++ data.sql
@@ -1,14 +1,111 @@
 /* Populate database with sample data. */
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Agumon', '03-02-2020', 0, true, 10.23);
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Agumon', '03-02-2020', 0, true, 10.23);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Gabumon', '15-11-2018', 2, true, 8);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Pikachu', '07-01-2021', 1, false, 15.04);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Devimon', '12-05-2017', 5, true, 11);
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Gabumon', '15-11-2018', 2, true, 8);
+
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Pikachu', '07-01-2021', 1, false, 15.04);
+
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Devimon', '12-05-2017', 5, true, 11);
 
 /*NEW DATA*/
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Plantmon', '15-11-2021', 2, true, -5.7);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Squirtle', '02-04-1993', 3, false, -12.13);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Angemon', '12-06-2005', 1, true, -45);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Boarmon', '07-06-2005', 7, true, 20.4);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Blossmon', '13-10-1998', 3, true, 17);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weigth_kg) VALUES ('Ditto', '14-05-2022', 4, true, 22);
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Plantmon', '15-11-2021', 2, true, -5.7);
+
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Squirtle', '02-04-1993', 3, false, -12.13);
+
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Angemon', '12-06-2005', 1, true, -45);
+
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Boarmon', '07-06-2005', 7, true, 20.4);
+
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Blossmon', '13-10-1998', 3, true, 17);
+
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    escape_attempts,
+    neutered,
+    weigth_kg
+  )
+VALUES
+  ('Ditto', '14-05-2022', 4, true, 22);
--- queries.sql
+++ queries.sql
@@ -1,92 +1,210 @@
 /*Queries that provide answers to the questions from all projects.*/
+SELECT
+  *
+FROM
+  animals
+WHERE
+  name LIKE '%mon%';
 
-SELECT * FROM animals WHERE name LIKE '%mon%';
-SELECT * FROM animals WHERE date_of_birth BETWEEN '01-01-2016' AND '31-12-2019';
-SELECT * FROM animals WHERE neutered = true AND escape_attempts < 3;
-SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
-SELECT name, escape_attempts FROM animals WHERE weigth_kg > 10.5;
-SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name != 'Gabumon';
-SELECT * FROM animals WHERE weigth_kg BETWEEN 10.4 AND 17.3;
+SELECT
+  *
+FROM
+  animals
+WHERE
+  date_of_birth BETWEEN '01-01-2016'
+  AND '31-12-2019';
+
+SELECT
+  *
+FROM
+  animals
+WHERE
+  neutered = true
+  AND escape_attempts < 3;
+
+SELECT
+  date_of_birth
+FROM
+  animals
+WHERE
+  name = 'Agumon'
+  OR name = 'Pikachu';
+
+SELECT
+  name,
+  escape_attempts
+FROM
+  animals
+WHERE
+  weigth_kg > 10.5;
+
+SELECT
+  *
+FROM
+  animals
+WHERE
+  neutered = true;
+
+SELECT
+  *
+FROM
+  animals
+WHERE
+  name != 'Gabumon';
+
+SELECT
+  *
+FROM
+  animals
+WHERE
+  weigth_kg BETWEEN 10.4
+  AND 17.3;
 
 /*MILESTONE-2*/
-
 -- Inside a transaction update the animals table by setting the species column to unspecified. 
 BEGIN;
-UPDATE animals SET species = 'unspecified';
+
+UPDATE
+  animals
+SET
+  species = 'unspecified';
 
 -- Verify that change was made. 
-SELECT * FROM animals;
+SELECT
+  *
+FROM
+  animals;
 
 -- Then roll back the change and verify that species columns went back to the state before transaction.
 ROLLBACK;
-SELECT * FROM animals;
 
-
+SELECT
+  *
+FROM
+  animals;
 
 -- Inside a transaction:
 -- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
 BEGIN;
-UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon%';
+
+UPDATE
+  animals
+SET
+  species = 'digimon'
+WHERE
+  name LIKE '%mon%';
 
 -- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
-UPDATE animals SET species = 'pokemon' WHERE species is NULL;
+UPDATE
+  animals
+SET
+  species = 'pokemon'
+WHERE
+  species is NULL;
 
 -- Commit the transaction.
 COMMIT;
 
 -- Verify that change was made and persists after commit.
-SELECT * FROM animals;
-
+SELECT
+  *
+FROM
+  animals;
 
 -- Now, take a deep breath and... Inside a transaction delete all records in the animals table, then roll back the transaction.
 BEGIN;
-DELETE FROM animals;
+
+DELETE FROM
+  animals;
+
 ROLLBACK;
 
 -- After the roll back verify if all records in the animals table still exist. After that you can start breath as usual ;)
-SELECT * FROM animals;
-
-
+SELECT
+  *
+FROM
+  animals;
 
 -- Inside a transaction:
 -- Delete all animals born after Jan 1st, 2022.
 BEGIN;
-DELETE FROM animals WHERE date_of_birth > '01-01-2022';
+
+DELETE FROM
+  animals
+WHERE
+  date_of_birth > '01-01-2022';
 
 -- Create a savepoint for the transaction.
 SAVEPOINT SP1;
 
 -- Update all animals' weight to be their weight multiplied by -1.
-UPDATE animals SET weigth_kg = weigth_kg *- 1;
+UPDATE
+  animals
+SET
+  weigth_kg = weigth_kg * - 1;
 
 -- Rollback to the savepoint
 ROLLBACK TO SP1;
 
 -- Update all animals' weights that are negative to be their weight multiplied by -1.
-UPDATE animals SET weigth_kg = weigth_kg * -1 WHERE weigth_kg < 0;
+UPDATE
+  animals
+SET
+  weigth_kg = weigth_kg * -1
+WHERE
+  weigth_kg < 0;
 
 -- Commit transaction
 COMMIT;
 
-
-
-
 -- Write queries to answer the following questions:
 -- How many animals are there?
-SELECT COUNT(*) FROM animals;
+SELECT
+  COUNT(*)
+FROM
+  animals;
 
 -- How many animals have never tried to escape?
-SELECT * FROM animals WHERE escape_attempts = 0;
+SELECT
+  *
+FROM
+  animals
+WHERE
+  escape_attempts = 0;
 
 -- What is the average weight of animals?
-SELECT AVG(weigth_kg) FROM animals;
+SELECT
+  AVG(weigth_kg)
+FROM
+  animals;
 
 -- Who escapes the most, neutered or not neutered animals?
-SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
+SELECT
+  neutered,
+  SUM(escape_attempts)
+FROM
+  animals
+GROUP BY
+  neutered;
 
 -- What is the minimum and maximum weight of each type of animal?
-SELECT species, MIN(weigth_kg), MAX(weigth_kg) FROM animals GROUP BY species;
+SELECT
+  species,
+  MIN(weigth_kg),
+  MAX(weigth_kg)
+FROM
+  animals
+GROUP BY
+  species;
 
 -- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
-SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '01-01-1990' AND '31-12-2000' GROUP BY species;
+SELECT
+  species,
+  AVG(escape_attempts)
+FROM
+  animals
+WHERE
+  date_of_birth BETWEEN '01-01-1990'
+  AND '31-12-2000'
+GROUP BY
+  species;
